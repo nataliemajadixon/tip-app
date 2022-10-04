b1618061452ba0d268971ab7ada7ee1bd4f4e2c1
natolay <ndixon@seattleu.edu>
2022-10-04 03:07:53 +0200

    added tip calculation function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let inputEl = document.querySelector("#bill-amount");
 let buttonEl = document.querySelector("#calculate-tip");
-let tipamountEl = document.querySelector("#tip-amount");
+let tipContainer = document.querySelector("#tip-amount");
+let tipAmount = 0;
 
-buttonEl.addEventListener("click", );
+function handleTipCalculation(){
+    
+console.log(inputEl.value)
+
+tipAmount = inputEl.value * .12;
+
+tipContainer.innerHTML = "You should tip " + "$" + tipAmount.toFixed(2)
+
+}
+
+buttonEl.addEventListener("click", handleTipCalculation);
