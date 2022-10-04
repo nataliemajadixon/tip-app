4a72b82357a553912e38266a8ef9a59309001fce
natolay <ndixon@seattleu.edu>
2022-10-04 14:02:24 +0200

    added 15 and 20 percent

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 let inputEl = document.querySelector("#bill-amount");
-let buttonEl = document.querySelector("#calculate-tip");
-let tipContainer = document.querySelector("#tip-amount");
-let tipAmount = 0;
+let buttonEl1 = document.querySelector("#calculate-tip12");
+let tipContainer12 = document.querySelector("#tip-amount12");
+let tipAmount12 = 0;
+let buttonEl2 = document.querySelector("#calculate-tip15");
+let tipContainer15 = document.querySelector("#tip-amount15");
+let tipAmount15 = 0;
+let buttonEl3 = document.querySelector("#calculate-tip20");
+let tipContainer20 = document.querySelector("#tip-amount20");
+let tipAmount20 = 0;
 
-function handleTipCalculation(){
+
+
+function handleTipCalculation12(){
     
-console.log(inputEl.value)
+    console.log(inputEl.value)
 
-tipAmount = inputEl.value * .12;
+    tipAmount12 = inputEl.value * .12;
 
-tipContainer.innerHTML = "You should tip " + "$" + tipAmount.toFixed(2)
+    tipContainer12.innerHTML = "You should tip " + "$" + tipAmount12.toFixed(2)
 
 }
 
-buttonEl.addEventListener("click", handleTipCalculation);
+    buttonEl1.addEventListener("click", handleTipCalculation12);
+
+function handleTipCalculation15(){
+   
+    tipAmount15 = inputEl.value * .15;
+
+    tipContainer15.innerHTML = "You should tip " + "$" + tipAmount15.toFixed(2)
+
+}
+
+    buttonEl2.addEventListener("click", handleTipCalculation15);
+
+function handleTipCalculation20(){
+
+    tipAmount20 = inputEl.value * .20;
+    
+    tipContainer20.innerHTML = "You should tip " + "$" + tipAmount20.toFixed(2)
+}
+    buttonEl3.addEventListener("click", handleTipCalculation20);
