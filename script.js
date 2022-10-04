3bd36c363357891a78a1b9cc1d558a16a8f20eb6
natolay <ndixon@seattleu.edu>
2022-10-04 02:31:14 +0200

    use queryselector to grab dom

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputEl = document.querySelector("#bill-amount");
+let buttonEl = document.querySelector("#calculate-tip");
+let tipamountEl = document.querySelector("#tip-amount");
+
+buttonEl.addEventListener("click", );
